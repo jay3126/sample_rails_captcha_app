bf40d176a0864061875c730d716fe967d1b9e5c5
Janmejay Rai <janmejay.rai@rsystems.com>
2014-03-21 13:39:12 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleRailsCaptchaApplication::Application.config.secret_key_base = '89a508605668aaf6bd85f7ce510864d6e958963fe38fd56ddae7cc65bc9c4cda4b671464fb6cdb49781874a42d622b9a0bbb150f7288bf1e2e3eab43c596b248'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleRailsCaptchaApplication::Application.config.session_store :cookie_store, key: '_sample_rails_captcha_application_session'
